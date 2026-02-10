cf7e6427095c69dcd1a1640f6c974166868d4a7a
Georgeikani <ikanigeorge2@gmail.com>
2026-02-10 09:02:22 +0000

    Display all food from database on manage food page

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -6,55 +6,85 @@ include 'partials/menu.php'
 <div class="main-content">
   <div class="wrapper">
     <h1>Manage Food</h1>
-    <br/><br/>
+    <br /><br />
 
-     <a class="btn-primary" href="<?= ROOT_URL ?>admin/add-food.php">Add Food</a>
+    <a class="btn-primary" href="<?= ROOT_URL ?>admin/add-food.php">Add Food</a>
 
-    <br/><br/>
+    <br /><br />
     <?php
-       if (isset($_SESSION['added']))
-            echo  $_SESSION['added'];
-            unset($_SESSION['added']);
+    if (isset($_SESSION['added']))
+      echo  $_SESSION['added'];
+    unset($_SESSION['added']);
 
     ?>
 
     <table class="tbl_full">
       <tr>
         <th>S.N</th>
-        <th>Full name</th>
-        <th>Username</th>
+        <th>Title</th>
+        <th>Price</th>
+        <th>Image</th>
+        <th>Featured</th>
+        <th>Active</th>
         <th>Actions</th>
       </tr>
+      <?php
+      //Create sql query to get all the food 
+      $sql = "SELECT * FROM tbl_food";
 
-      <tr>
-        <td>1.</td>
-        <td>George_Ikani</td>
-        <td>George Ibembem</td>
-        <td>
-          <a class="btn-secondary" href="">Update Admin</a>
-          <a class="btn-danger" href="">Delete Admin</a>
-        </td>
-      </tr>
+      //Execute the query
+      $res = mysqli_query($connection, $sql);
+      //Count rows to check if we have food or not
+      $count = mysqli_num_rows($res);
 
-      <tr>
-        <td>2.</td>
-        <td>George_Ikani</td>
-        <td>George Ibembem</td>
-        <td>
-          <a class="btn-secondary" href="">Update Admin</a>
-          <a class="btn-danger" href="">Delete Admin</a>
-        </td>
-      </tr>
+      //Create serial number variable and set default value
+      $sn = 1;
 
-      <tr>
-        <td>3.</td>
-        <td>George_Ikani</td>
-        <td>George Ibembem</td>
-        <td>
-          <a class="btn-secondary" href="">Update Admin</a>
-          <a class="btn-danger" href="">Delete Admin</a>
-        </td>
-      </tr>
+      if ($count > 0) {
+        //we have food in database
+        //Get the food from DB and display
+        while ($row = mysqli_fetch_assoc($res)) {
+          //get the values for individual column
+          $id = $row['id'];
+          $title = $row['title'];
+          $price = $row['price'];
+          $image_name = $row['image_name'];
+          $featured = $row['featured'];
+          $active = $row['active'];
+
+      ?>
+          <tr>
+            <td><?= $sn++ ?></td>
+            <td><?= $title ?></td>
+            <td><?= $price ?></td>
+            <td>   <?php 
+                //check if whether image is available
+                if($image_name!=""){
+                  //display image
+                  ?>
+                      <img src="<?= ROOT_URL ?>images/food/<?= $image_name ?>" width="30px">
+                  <?php
+                }else{
+                  echo "<div class='alert-mesaage error'>Image not added</div>";
+                }
+
+               ?></td>
+            <td><?= $featured ?></td>
+            <td><?= $active ?></td>
+
+            <td class="actions">
+              <a class="btn-secondary" href="">Update Food</a>
+              <a class="btn-danger" href="">Delete Food</a>
+            </td>
+          </tr>
+      <?php
+        }
+      } else {
+        //Food not added to database
+        echo "<tr> <td colspan='7' class='alert-message error'>Food not Added Yet</td> </tr>";
+      }
+
+      ?>
 
     </table>
   </div>
